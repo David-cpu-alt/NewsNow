ba0540115275d57941942e6d4f50bb418513c3de
David-cpu-alt <davidoladejo2020@gmail.com>
2024-03-09 14:13:54 +0100

    saturday commit

--- src/screens/Main/Home/Home.jsx
+++ src/screens/Main/Home/Home.jsx
@@ -1,13 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 import Toptab from '../../../navigators/Tab/Toptab/Toptab'
+import { SIZES, images, icons } from '../../../constants'
 
 
 
 const Home = () => {
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+            <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                <Image source={images.news} style={{ height: SIZES.h2, width: SIZES.h2, }} />
+                <Text style={{ marginRight: SIZES.h1 * 6, fontWeight: "bold" }}> Nows.co</Text>
+                <Image source={icons.search} style={{ height: SIZES.h3, width: SIZES.h3, marginRight: SIZES.h6 }} />
+                <Image source={images.circle} style={{ height: SIZES.h1 * 1.2, width: SIZES.h1 * 1.2 }} />
+            </View>
+            {/* carousel */}
+
+
             <Toptab />
 
         </View>
@@ -19,6 +28,7 @@ export default Home
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "white",
-        flex: 1
+        flex: 1,
+        padding: SIZES.h5
     }
 })
